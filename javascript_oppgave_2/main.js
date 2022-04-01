2dc3e62ba73ba7d08df56dea97ceaeee38e1260e
hardstein <89636138+hardstein@users.noreply.github.com>
2022-04-01 21:57:03 +0200

    Gjort styling og mye av funksjonaliteten

--- javascript_oppgave_2/main.js
+++ javascript_oppgave_2/main.js
@@ -1,7 +1,5 @@
 // Start koding lengre ned (ved startHere)
 
-
-
 const failColor = "rgb(226, 101, 91)";
 const correctColor = "rgb(25, 232, 168)";
 const failText = "Feil";
@@ -35,5 +33,82 @@ const wordList = [
   "Dra",
 ];
 
+// const wordList = ["d", "e", "z", "n", "c", "a", "b"];
+
 // #### START HER ####
-const startHere = "Her kommer din kode";
+const inpText = document.getElementsByTagName("input");
+const spans = document.getElementsByTagName("span");
+const btnTest = document.getElementById("test");
+
+let fourWords = [];
+let correct = [];
+
+const wrong = () => {
+  btnTest.style.backgroundColor = failColor;
+  btnTest.innerHTML = failText;
+};
+
+const next = () => {
+  btnTest.style.backgroundColor = correctColor;
+  btnTest.innerHTML = correctText;
+  // FALSK STOPP
+  setTimeout(() => {genereateWords()}, 2000);
+  // btnTest.addEventListener("click", genereateWords);
+  // genereateWords();
+};
+
+const genereateWords = () => {
+  btnTest.style.backgroundColor = "var(--white)";
+  btnTest.innerHTML = "Test";
+  fourWords = [];
+  correct = [];
+  for (let i = 0; i < 4; i++) {
+    var randomWord = Math.floor(Math.random() * wordList.length);
+    fourWords.push(wordList[randomWord]);
+    spans[i].innerHTML = wordList[randomWord];
+  }
+  console.log("words :", fourWords);
+  checkOrder();
+};
+
+const checkOrder = () => {
+  let copyFourWords = fourWords.slice();
+  correct = copyFourWords.sort();
+  console.log("Sortet: ", correct);
+};
+
+const validate = () => {
+  let inpTextToArray = [...inpText];
+  let result = true;
+  for (let i = 0; i < inpTextToArray.length; i++) {
+    if (inpTextToArray[i].value > 4) {
+      console.log("TALLET MÅ VÆRE MINDRE");
+      result = false;
+    }
+
+    if (inpTextToArray[i].value < 1) {
+      console.log("TALLET MÅ VÆRE STØRRE");
+      result = false;
+    }
+
+    if (isNaN(inpTextToArray[i].value)) {
+      console.log("KUN TALL");
+      result = false;
+    }
+
+    // Hvis ikke alle er rett
+    if (correct.indexOf(fourWords[i]) != inpTextToArray[i].value - 1) {
+      result = false;
+    }
+  }
+
+  console.log(result);
+
+  result ? next() : wrong();
+};
+
+window.onload = function () {
+  genereateWords();
+};
+
+btnTest.addEventListener("click", validate);
