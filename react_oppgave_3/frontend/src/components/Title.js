0d3a859e3d8d0d9a6dd239614a9133b975409cae
hardstein <89636138+hardstein@users.noreply.github.com>
2022-05-13 13:16:05 +0200

    Gjort om filer fra jsx til js

--- react_oppgave_3/frontend/src/components/Title.js
+++ react_oppgave_3/frontend/src/components/Title.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Title( {text, testid}) {
   return (
-    // Forstår at det er gunstig å ha hvis det er en tekst som er lik på flere steder/sider.
+    // Lite brukt, men forstår at det er gunstig å ha hvis det er en tekst som er lik på flere steder/sider.
     <h2 data-testid={testid}  className='title'>{text}</h2>
   )
 }
